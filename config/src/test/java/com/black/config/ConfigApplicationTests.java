46140cf8cccdf18b29085c86939caf4f05da689f
black82 <blackrailean@gmail.com>
2021-03-22 22:53:03 +0200

    first commit

---
+++ config/src/test/java/com/black/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
